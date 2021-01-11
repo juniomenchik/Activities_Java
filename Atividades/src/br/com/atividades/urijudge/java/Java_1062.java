a9ed3551b2aafce31a3ebd5ce9b789a518abc0c5
JeffMenchik <juniomenchik@live.com>
2021-01-11 10:49:47 -0300

    dakle

---
+++ Atividades/src/br/com/atividades/urijudge/java/Java_1062.java
@@ -0,0 +1,33 @@
+package br.com.atividades.urijudge.java;
+
+import java.util.Scanner;
+
+public class Java_1062 {
+
+	public static void main(String[] args) {
+		
+		Scanner x = new Scanner(System.in);
+		
+		String entradalaco = x.nextLine();
+		
+		System.out.println(entradalaco);
+		char[] novo = entradalaco.toCharArray();
+		
+		System.out.println(novo[3]);
+//		while(entradalaco != 0) {
+//			
+//			for(int i = 0 ; i<entradalaco;i++) {
+//				String esquerdaDireita = 
+//				
+//				
+//			}
+//			
+//			
+//		}
+		
+		
+		x.close();
+		
+	}
+	
+}
---
+++ Atividades/src/br/com/atividades/urijudge/java/TesteString.java
@@ -0,0 +1,27 @@
+package br.com.atividades.urijudge.java;
+
+/**
+ * @author Jeff Menchik
+ *
+ */
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testando Metodos da classe String.");
+		
+		String nome = "Jeferson";
+		System.out.println("String nome : " + nome );
+		System.out.println("Testando .charAt(5)   =  " + nome.charAt(5));
+		
+		System.out.println("Testando .codePointAt(1) e (2)  =  " 
+		+ nome.codePointAt(1) + " "+ nome.codePointAt(2));
+		
+		System.out.println("Testando .codePointBefore(2) e (3)  =  " 
+		+ nome.codePointBefore(2) + " " + nome.codePointBefore(3));
+		
+		System.out.println("Testando nome.length() =    " + nome.length());
+		
+	}
+	
+}
