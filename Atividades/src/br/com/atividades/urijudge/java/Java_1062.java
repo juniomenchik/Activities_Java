a2ff441aaf1a6d19bb7f8b439ef1fba53204368c
JeffMenchik <juniomenchik@live.com>
2021-01-11 15:41:08 -0300

    stringsnote

--- Atividades/src/br/com/atividades/urijudge/java/Java_1062.java
+++ Atividades/src/br/com/atividades/urijudge/java/Java_1062.java
@@ -7,32 +7,43 @@ public class Java_1062 {
 	public static void main(String[] args) {
 		int entradalac;
 		Scanner x = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		entradalac = x.nextInt();
-		
-			for(int y=0;y!=entradalac;entradalac=x.nextInt()) {
-				String crescente = "";
-				String decrescente = "";
-				for (int i = 0; i <= entradalac; i++) {
 
-					crescente = crescente.concat(Integer.toString(i));
+		for (int y = 0; y != entradalac; entradalac = x.nextInt()) {
+			String crescente = "";
+			String decrescente = "";
+			String comparacao = "";
+			for (int i = 1; i <= entradalac; i++) {
 
-					if (i == entradalac) {
-						System.out.println(crescente);
-					} else
-						crescente = crescente.concat(" ");
-				}
-				for (int i = entradalac; i >= 0; i--) {
+				crescente = crescente.concat(Integer.toString(i));
 
-					decrescente = decrescente.concat(Integer.toString(i));
-
-					if (i == 0) {
-						System.out.println(decrescente);
-					} else
-						decrescente = decrescente.concat(" ");
-				}
+				if (i == entradalac) {
+				} else
+					crescente = crescente.concat(" ");
 			}
-		
+			for (int i = entradalac; i > 0; i--) {
 
+				decrescente = decrescente.concat(Integer.toString(i));
+
+				if (i == 1) {
+				} else
+					decrescente = decrescente.concat(" ");
+			}
+			comparacao = scanner.nextLine();
+			for (String j = "0"; j.compareToIgnoreCase(comparacao) != 0; comparacao = scanner.nextLine()) {
+				if (comparacao.compareToIgnoreCase(crescente)==0) {
+					System.out.println("yes");
+				} else
+					if(comparacao.compareToIgnoreCase(decrescente)==0){
+					System.out.println("yes");	
+					}else System.out.println("no");
+				scanner.reset();
+			}
+			x.reset();
+		}
+
+		scanner.close();
 		x.close();
 	}
 
--- Atividades/src/br/com/atividades/urijudge/java/TesteString.java
+++ Atividades/src/br/com/atividades/urijudge/java/TesteString.java
@@ -11,16 +11,19 @@ public class TesteString {
 		System.out.println("Testando Metodos da classe String.");
 		
 		String nome = "Jeferson";
-		System.out.println("String nome : " + nome );
-		System.out.println("Testando .charAt(5)   =  " + nome.charAt(5));
-		
-		System.out.println("Testando .codePointAt(1) e (2)  =  " 
-		+ nome.codePointAt(1) + " "+ nome.codePointAt(2));
-		
-		System.out.println("Testando .codePointBefore(2) e (3)  =  " 
-		+ nome.codePointBefore(2) + " " + nome.codePointBefore(3));
-		
-		System.out.println("Testando nome.length() =    " + nome.length());
+		String dale = "Jeferson";
+//		System.out.println("String nome : " + nome );
+//		System.out.println("Testando .charAt(5)   =  " + nome.charAt(5));
+//		
+//		System.out.println("Testando .codePointAt(1) e (2)  =  " 
+//		+ nome.codePointAt(1) + " "+ nome.codePointAt(2));
+//		
+//		System.out.println("Testando .codePointBefore(2) e (3)  =  " 
+//		+ nome.codePointBefore(2) + " " + nome.codePointBefore(3));
+//		
+//		System.out.println("Testando nome.length() =    " + nome.length());
+//		
+		System.out.println(nome.compareToIgnoreCase(dale));
 		
 	}
 	
