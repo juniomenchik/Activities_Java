c16827ce6bcfac596b422eadc65af1bd37c64fe9
JeffMenchik <juniomenchik@live.com>
2021-01-13 15:40:14 -0300

    MEODEOS

--- Atividades/src/br/com/atividades/urijudge/java/Java_1166.java
+++ Atividades/src/br/com/atividades/urijudge/java/Java_1166.java
@@ -9,31 +9,46 @@ public class Java_1166 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		int numdepilhas = 5;
-		Stack<Integer> pilhas[] = new Stack[numdepilhas];
-		for (int i = 0; i < numdepilhas; i++) {
-			pilhas[i] = new Stack<>();
-		}
-		int bola = 1;
-		for (int vara = 0; vara < numdepilhas;) {
+		String sisi = scanner.nextLine();
+		for (int n = Integer.parseInt(sisi); n > 0; n--) {
+			String numdepilha = scanner.nextLine();
+			int numdepilhas = Integer.parseInt(numdepilha);
+			scanner.reset();
+			Stack<Integer> pilhas[] = new Stack[numdepilhas];
+			for (int i = 0; i < numdepilhas; i++) {
+				pilhas[i] = new Stack<>();
+			}
+			int bola = 1;
+			for (int vara = 0; vara < numdepilhas;) {
 
-			if (pilhas[vara].empty()) {
-				
-				pilhas[vara].push(bola);
-				bola++;
-			} else {
-				
-				int ultimabola = pilhas[vara].peek();
-				double soma = bola + ultimabola;
-				soma = Math.sqrt(soma);
-				if (soma == (int) soma) {
+				if (pilhas[vara].empty()) {
+
 					pilhas[vara].push(bola);
 					bola++;
-				} else
-					vara++;
-			}
+					vara = 0;
+				} else {
 
+					int ultimabola = pilhas[vara].peek();
+					double soma = bola + ultimabola;
+					soma = Math.sqrt(soma);
+					if (soma == (int) soma) {
+						pilhas[vara].push(bola);
+						bola++;
+						vara = 0;
+					} else
+						vara++;
+				}
+
+			}
+			if(numdepilhas>50) {
+				System.out.println(-1);
+			}else {
+				System.out.println(bola - 1);
+			}
+			
 		}
+
+		System.out.println();
 
 		scanner.close();
 	}
