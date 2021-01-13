7d52191d1ca58a24f651ebfbd931be747fbeaffe
JeffMenchik <juniomenchik@live.com>
2021-01-13 10:33:57 -0300

    Atividade 1036
    Atividade de bhascara
     - Scanner
     - Double
     - Raiz    -     Potencia
    NaN  ?

--- Atividades/src/br/com/atividades/urijudge/java/Java_1036.java
+++ Atividades/src/br/com/atividades/urijudge/java/Java_1036.java
@@ -7,17 +7,23 @@ public class Java_1036 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		double a = 10.0;
-		double b = 20.1;
-		double c = 5.1;
+		double a = 0.0;
+		double b = 0.0;
+		double c = 0.0;
 		
 		double r1 = 0.0;
 		double r2 = 0.0;
 		
 		String str = scanner.nextLine();
-		String arstr []= str.split(" ");
 		
+		String arstr[]= str.split("\\s");
 		
+		a = Double.parseDouble(arstr[0]);
+		b = Double.parseDouble(arstr[1]);
+		c = Double.parseDouble(arstr[2]);
+		
+			
+			
 		
 		
 		double delta = (b*b);
@@ -30,13 +36,19 @@ public class Java_1036 {
 		r2 = -b - Math.sqrt(delta);
 		r1 = r1 / (2*a);
 		r2 = r2 / (2*a);
-		System.out.printf("R1 = %.5f %n",r1);
-		System.out.printf("R2 = %.5f %n",r2);
+		if( Double.isNaN(r1) ||  Double.isNaN(r2) ) {
+			System.out.println("Impossivel calcular");
+		}else {
+			System.out.printf("R1 = %.5f%n",r1);
+			System.out.printf("R2 = %.5f%n",r2);
+		}
+		
 		//   RAIZ         Math.sqrt(a)
 		// 					Math.pow(var,exp)
 		// 					Math.pow(5, 2)
-		
+
 		scanner.close();
+		
 	}
 	
 }
