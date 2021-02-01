070f2e127e82938c6c09a43d9e9ffce9010c5a19
JeffMenchik <juniomenchik@live.com>
2021-02-01 07:14:42 -0300

    Update Java_1242.java

--- Atividades/src/br/com/atividades/urijudge/java/Java_1242.java
+++ Atividades/src/br/com/atividades/urijudge/java/Java_1242.java
@@ -8,29 +8,28 @@ import java.util.Stack;
 
 public class Java_1242 {
 
-    static BufferedReader leitura = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
 
-        String rna;
-        while ((rna = leitura.readLine()) != null) { // leitura ate o EOF
+        String entrada;
+        while ((entrada = input.readLine()) != null) { 
             int ligacoes = 0;
-            char[] rnaChar = rna.toCharArray(); // passa a strinf para um array de char
-            Stack<Byte> pilha = new Stack<>(); // este array de char vai ser colocado em uma pilha para verificacao de conexoes
-
-            for (int i = 0; i < rna.length(); i++) {
-                if (!pilha.empty() && conexao((byte) rnaChar[i], pilha.peek())) {
-                    ligacoes++; // condicao para haver uma conexao é a funcao retornar true e a pilha nao estar vazia
-                    pilha.pop(); // retira o primeiro elemento da pilha
+            char[] charEntrada = entrada.toCharArray();
+            Stack<Byte> pilha = new Stack<>();
+            for (int i = 0; i < entrada.length(); i++) {
+                if (!pilha.empty() && temConexao((byte) charEntrada[i], pilha.peek())) {
+                    ligacoes++;
+                    pilha.pop();
                 } else {
-                    pilha.push((byte) rnaChar[i]); // adiciona o char[i] na pilha
+                    pilha.push((byte) charEntrada[i]); 
                 }
             }
             System.out.println(ligacoes);
         }
     }
 
-    public static boolean conexao(byte a, byte b) {
+    public static boolean temConexao(byte a, byte b) {
         return (a == 'B' && b == 'S') || (b == 'B' && a == 'S') || (a == 'C' && b == 'F') || (b == 'C' && a == 'F'); //retorna se existe a conexao ou nao 
     }
 }
